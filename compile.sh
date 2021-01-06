2883199463ff7c72ab5a6f29e2b9163fc66f57e3
ashawkey <963113090@qq.com>
2021-01-06 16:28:19 +0800

    delete single thread

--- compile.sh
+++
@@ -1,1 +0,0 @@
-g++ depth2normal.cpp -o depth2normal -std=c++11 -I ./include/ -Wall -O3 `pkg-config opencv --cflags --libs`
--- run.sh
+++
@@ -1,1 +0,0 @@
-./depth2normal data_json/kitti_dc.json /data/kitti_depth/ /data/kitti_normal/
